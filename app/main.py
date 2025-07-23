5d2ca47518271bf66f12dbab34a396e923d183a5
tangshoubin <shoubin.tang@gmail.com>
2025-07-23 22:50:36 +0800

    update

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import Depends, FastAPI
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from .routers import items, users
+from config.config import settings
 
 # app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
