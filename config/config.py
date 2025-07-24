133707fcd3d20f5e62667ad58d4103f54df48a8c
tangshoubin <shoubin.tang@gmail.com>
2025-07-24 15:39:34 +0800

    update

--- config/config.py
+++ config/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     admin_email: str
     items_per_user: int = 50
 
-    database_url: str = "mysql+pymysql://user:password@localhost/dbname"
+    database_url: str = "mysql+pymysql://root:mypassword@127.0.0.1/test2"
     database_echo: bool = True
 
     class Config:
