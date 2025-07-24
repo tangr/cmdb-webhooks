33ea6fe9d1dd132346c8f05a8a4bdf830a92125c
tangshoubin <shoubin.tang@gmail.com>
2025-07-24 11:14:09 +0800

    update

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,4 +1,10 @@
 from fastapi import Header, HTTPException
+from config.config import get_session
+from typing import Annotated
+from sqlmodel import Session
+from fastapi import Depends
+
+SessionDep = Annotated[Session, Depends(get_session)]
 
 
 async def get_token_header(x_token: str = Header()):
