26144b9df62d3878273450a6d59e591100c430b5
tangshoubin <shoubin.tang@gmail.com>
2025-07-24 11:14:20 +0800

    update

--- config/config.py
+++ config/config.py
@@ -1,10 +1,15 @@
 from pydantic_settings import BaseSettings
+from sqlmodel import create_engine, Session
+from typing import Generator
 
 
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
     admin_email: str
     items_per_user: int = 50
+
+    database_url: str = "mysql+pymysql://user:password@localhost/dbname"
+    database_echo: bool = True
 
     class Config:
         env_file = ".env"
@@ -13,3 +18,10 @@ class Settings(BaseSettings):
 
 
 settings = Settings()
+
+engine = create_engine(settings.database_url, echo=settings.database_echo)
+
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
