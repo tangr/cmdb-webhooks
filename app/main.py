935ce7c1eb6df0e803df8aad4e5d448fc82e4972
tangshoubin <shoubin.tang@gmail.com>
2025-07-24 11:45:32 +0800

    update

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI
 
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
-from .routers import items, users
+from .routers import items, users, jms_reqlog
 from config.config import settings
 
 # app = FastAPI(dependencies=[Depends(get_query_token)])
@@ -11,6 +11,7 @@ app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(items.router)
+app.include_router(jms_reqlog.router)
 app.include_router(
     admin.router,
     prefix="/admin",
