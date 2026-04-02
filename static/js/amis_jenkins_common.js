7561a0de52da28cde112556958d44bb26d48fd3e
tangshoubin <shoubin.tang@gmail.com>
2026-04-02 23:45:52 +0800

    update change to amis form from Semantic UI by claude code

--- static/js/amis_jenkins_common.js
+++ static/js/amis_jenkins_common.js
@@ -10,10 +10,7 @@
  */
 var AmisJenkins = (function () {
   var _onSuccess = null;
-  var _currentModalJobId = null;
-  var _currentModalJobData = null;
   var _amisFormInstance = null;
-  var _amisFormData = {};
 
   function init(onSuccessCallback) {
     _onSuccess = onSuccessCallback;
@@ -46,31 +43,33 @@ var AmisJenkins = (function () {
           alert("Failed to load job details: " + result.msg);
           return;
         }
-        _currentModalJobId = jobId;
-        _currentModalJobData = result.data;
-        _populateExecuteModal(result.data);
+        _populateExecuteModal(jobId, result.data);
         $("#execute-modal").modal({
           closable: true,
-          onApprove: function () {
-            _executeJobFromModal();
-            return false;
-          },
           onHidden: function () {
             if (_amisFormInstance) {
               _amisFormInstance.unmount();
               _amisFormInstance = null;
             }
             $("#amis-form-container").html("");
-            window.__amisExecuteFormData = null;
+            // Unbind execute button click
+            $("#modal-execute-btn").off("click");
           }
         }).modal("show");
+        // Bind execute button to trigger Amis form submit via hidden button
+        $("#modal-execute-btn").off("click").on("click", function () {
+          if (!confirm("Are you sure you want to execute this Jenkins job?")) return;
+          $("#modal-execute-btn").addClass("loading disabled");
+          var hiddenBtn = document.querySelector("#amis-form-container .amis-hidden-submit");
+          if (hiddenBtn) hiddenBtn.click();
+        });
       })
       .catch(function (error) {
         alert("Error loading job: " + error.message);
       });
   }
 
-  function _populateExecuteModal(job) {
+  function _populateExecuteModal(jobId, job) {
     // Job info header
     var infoHtml = '<div class="ui list">';
     infoHtml += '<div class="item"><strong>Form:</strong> ' + escapeHtml(job.form_title) + '</div>';
@@ -91,6 +90,7 @@ var AmisJenkins = (function () {
     // Get field schema (includes all fields, non-modifiable marked as static)
     var formSchemaConfig = job.execution_form_schema || {};
     var fieldSchema = formSchemaConfig.schema || [];
+    var modifiableFields = formSchemaConfig.fields || [];
     var requestParams = job.request_params || {};
 
     // Clean up previous Amis instance
@@ -102,6 +102,33 @@ var AmisJenkins = (function () {
     if (fieldSchema.length > 0) {
       $("#amis-form-container").html("");
 
+      // Build api.data mapping: only pick modifiable fields into modified_fields
+      var modifiedFieldsMapping = {};
+      modifiableFields.forEach(function (fieldName) {
+        modifiedFieldsMapping[fieldName] = "${" + fieldName + "}";
+      });
+
+      var apiConfig = {
+        method: "post",
+        url: "/amis-jenkins/api/pending/" + jobId + "/execute",
+        data: {
+          modified_fields: modifiedFieldsMapping
+        }
+      };
+
+      // Build success message script
+      // Amis puts response.data into event.data, so fields like execution_count, jenkins_response are directly accessible
+      var successScript =
+        '$("#modal-execute-btn").removeClass("loading disabled");' +
+        'var d = event.data || {};' +
+        'var msg = "Jenkins build triggered successfully!\\nExecution count: " + (d.execution_count || "");' +
+        'var jr = d.jenkins_response || {};' +
+        'if (jr._build_url) { msg += "\\n\\nBuild URL:\\n" + jr._build_url; }' +
+        'else if (jr._queue_url) { msg += "\\n\\nQueue URL (build not yet started):\\n" + jr._queue_url; }' +
+        'alert(msg);' +
+        '$("#execute-modal").modal("hide");' +
+        'if (window.__amisOnExecuteSuccess) { window.__amisOnExecuteSuccess(); }';
+
       var amisSchema = {
         type: "page",
         body: {
@@ -110,98 +137,86 @@ var AmisJenkins = (function () {
           mode: "horizontal",
           wrapWithPanel: false,
           submitOnChange: false,
+          api: apiConfig,
           actions: [],
           onEvent: {
-            change: {
+            submitSucc: {
               actions: [{
                 actionType: "custom",
-                script: "window.__amisExecuteFormData = Object.assign(window.__amisExecuteFormData || {}, event.data);"
+                script: successScript
+              }]
+            },
+            submitFail: {
+              actions: [{
+                actionType: "custom",
+                script: '$("#modal-execute-btn").removeClass("loading disabled"); var d = event.data || {}; var msg = d.msg || d.detail || "Unknown error"; alert("Failed to execute: " + msg);'
               }]
             }
           },
-          body: fieldSchema
+          body: fieldSchema.concat([
+            {
+              type: "button",
+              actionType: "submit",
+              label: "",
+              className: "amis-hidden-submit",
+              style: { position: "absolute", width: 0, height: 0, overflow: "hidden", opacity: 0 }
+            }
+          ])
         }
       };
 
-      _amisFormData = Object.assign({}, requestParams);
-      window.__amisExecuteFormData = Object.assign({}, requestParams);
+      // Register success callback for modal close
+      window.__amisOnExecuteSuccess = _onSuccess;
 
       var amis = amisRequire("amis/embed");
       _amisFormInstance = amis.embed(
         "#amis-form-container",
         amisSchema,
         {
-          data: _amisFormData
+          data: Object.assign({}, requestParams)
         },
         {
           theme: "cxd",
           locale: "zh-CN",
           getModalContainer: function () {
             return document.getElementById("amis-form-container");
+          },
+          fetcher: function (options) {
+            var method = (options.method || "GET").toUpperCase();
+            var headers = Object.assign({}, options.headers || {});
+            var fetchOptions = {
+              method: method,
+              headers: headers,
+              credentials: "include"
+            };
+
+            if (method !== "GET" && method !== "HEAD" && options.data) {
+              if (typeof options.data === "object") {
+                headers["Content-Type"] = "application/json";
+                fetchOptions.body = JSON.stringify(options.data);
+              } else {
+                fetchOptions.body = options.data;
+              }
+              fetchOptions.headers = headers;
+            }
+
+            return fetch(options.url, fetchOptions)
+              .then(function (response) {
+                return response.json().then(function (data) {
+                  return { status: response.status, data: data };
+                });
+              });
+          },
+          notify: function (type, msg) {
+            if (type === "error") {
+              console.error("[Amis Error]", msg);
+            }
           }
         }
       );
     } else {
       $("#amis-form-container").html("<p>No parameters to display</p>");
     }
-  }
-
-  function _executeJobFromModal() {
-    if (!_currentModalJobId) {
-      alert("No job selected");
-      return;
-    }
-
-    var modifiedFields = {};
-    var formSchemaConfig = _currentModalJobData.execution_form_schema || {};
-    var modifiableFields = formSchemaConfig.fields || [];
-
-    if (modifiableFields.length > 0) {
-      // Read current form values tracked by onEvent.change
-      var currentValues = window.__amisExecuteFormData || _amisFormData;
-
-      modifiableFields.forEach(function (fieldName) {
-        if (currentValues.hasOwnProperty(fieldName)) {
-          modifiedFields[fieldName] = currentValues[fieldName];
-        }
-      });
-    }
-
-    $("#modal-execute-btn").addClass("loading disabled");
-
-    var requestBody = {};
-    if (Object.keys(modifiedFields).length > 0) {
-      requestBody.modified_fields = modifiedFields;
-    }
-
-    fetch("/amis-jenkins/api/pending/" + _currentModalJobId + "/execute", {
-      method: "POST",
-      credentials: "include",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(requestBody)
-    })
-      .then(function (response) { return response.json(); })
-      .then(function (result) {
-        $("#modal-execute-btn").removeClass("loading disabled");
-        if (result.status === 0) {
-          $("#execute-modal").modal("hide");
-          var msg = "Jenkins build triggered successfully!\nExecution count: " + result.data.execution_count;
-          var jr = result.data.jenkins_response || {};
-          if (jr._build_url) {
-            msg += "\n\nBuild URL:\n" + jr._build_url;
-          } else if (jr._queue_url) {
-            msg += "\n\nQueue URL (build not yet started):\n" + jr._queue_url;
-          }
-          alert(msg);
-          if (_onSuccess) _onSuccess();
-        } else {
-          alert("Failed to execute: " + result.msg);
-        }
-      })
-      .catch(function (error) {
-        $("#modal-execute-btn").removeClass("loading disabled");
-        alert("Error: " + error.message);
-      });
   }
 
   // ==================== Utility Functions ====================
